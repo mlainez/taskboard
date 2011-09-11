ca0973923e286b96f757532aca26c9d5f32fef6a
Marc Lainez <marc.lainez@gmail.com>
2011-09-11 20:11:59 +0200

    Add deploy files

---
+++ config/deploy/production.rb
@@ -0,0 +1,48 @@
+require 'net/http'
+require 'uri'
+
+namespace :deploy do
+    set :application, "taskboard"
+
+    # If you aren't deploying to /u/apps/#{application} on the target
+    # servers (which is the default), you can specify the actual location
+    # via the :deploy_to variable:
+    set :deploy_to, "/var/www/rails/#{application}/production"
+
+    set :scm, :git
+    set :repository,  "git@github.com:mlainez/agilar-#{application}.git"
+    set :branch, "develop"
+    set :use_sudo, false
+
+    server "dev.agilar.org", :app, :web, :db, :primary => true
+
+    set :user, "deploy"
+
+   desc "Restarting mod_rails with restart.txt"
+   task :restart, :roles => :app, :except => { :no_release => true } do
+     run "touch #{current_path}/tmp/restart.txt"
+   end
+
+  # task :create_member_pictures_symlink do
+  #   run "ln -s #{deploy_to}/shared/images/members #{deploy_to}/current/public/images/"
+  # end
+
+  task :symlink_members_pictures do
+    run "ln -s #{deploy_to}/shared/system/ #{deploy_to}/current/public/system"
+  end
+
+  task :rake_db_migrate do
+    run "cd #{current_path}/ && rake RAILS_ENV=\"production\" db:migrate"
+  end
+
+  task :rake_db_seed do
+    run "cd #{current_path}/ && rake RAILS_ENV=\"production\" db:seed"
+  end
+
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
+
+  after "deploy:update", "deploy:rake_db_migrate", "deploy:rake_db_seed", "deploy:symlink_members_pictures"
+end
---
+++ config/deploy/testing.rb
@@ -0,0 +1,45 @@
+require 'uri'
+
+namespace :deploy do
+  set :application, "taskboard"
+
+  # If you aren't deploying to /u/apps/#{application} on the target
+  # servers (which is the default), you can specify the actual location
+  # via the :deploy_to variable:
+  set :deploy_to, "/var/www/rails/#{application}/testing"
+
+  set :scm, :git
+  set :repository,  "git@github.com:mlainez/agilar-#{application}.git"
+  set :branch, "develop"
+  set :use_sudo, false
+  set :user, 'deploy'
+
+  server "dev.agilar.org", :app, :web, :db, :primary => true
+
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+   run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  desc "Removng pictures"
+  task :remove_pictures do
+    run "cd #{current_path}/ && rm -rf public/system/*"
+  end
+
+  desc "Migrating database (reset)"
+  task :rake_db_migrate do
+    run "cd #{current_path}/ && rake RAILS_ENV=\"testing\" db:migrate:reset --trace"
+  end
+
+  desc "Seeding Database"
+  task :rake_db_seed do
+    run "cd #{current_path}/ && rake RAILS_ENV=\"testing\" db:seed --trace"
+  end
+
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
+
+  after "deploy:update", "deploy:remove_pictures", "deploy:rake_db_migrate", "deploy:rake_db_seed"
+end
